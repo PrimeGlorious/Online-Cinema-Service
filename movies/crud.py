d4c34c3534411ed4d6ffe3d8e9c6e349a4ca32c4
Polina Haltseva <polinayut@gmail.com>
2025-06-05 15:42:43 +0300

    Update crud.py

--- movies/crud.py
+++ movies/crud.py
@@ -1,40 +1,96 @@
+from typing import List, Optional
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from . import models, schemas
 
 
-def create_certification(db: Session, certification: schemas.CertificationCreate):
-    db_cert = models.Certification(**certification.dict())
-    db.add(db_cert)
-    db.commit()
-    db.refresh(db_cert)
-    return db_cert
+# --- Genres ---
+
+def get_genre(db: Session, genre_id: int) -> Optional[models.Genre]:
+    return db.query(models.Genre).filter(models.Genre.id == genre_id).first()
 
 
-def create_genre(db: Session, genre: schemas.GenreCreate):
-    db_genre = models.Genre(**genre.dict())
+def get_genres(db: Session, skip: int = 0, limit: int = 100) -> List[models.Genre]:
+    return db.query(models.Genre).offset(skip).limit(limit).all()
+
+
+def create_genre(db: Session, genre: schemas.GenreCreate) -> models.Genre:
+    db_genre = models.Genre(name=genre.name)
     db.add(db_genre)
     db.commit()
     db.refresh(db_genre)
     return db_genre
 
 
-def create_star(db: Session, star: schemas.StarCreate):
-    db_star = models.Star(**star.dict())
+# --- Stars ---
+
+def get_star(db: Session, star_id: int) -> Optional[models.Star]:
+    return db.query(models.Star).filter(models.Star.id == star_id).first()
+
+
+def get_stars(db: Session, skip: int = 0, limit: int = 100) -> List[models.Star]:
+    return db.query(models.Star).offset(skip).limit(limit).all()
+
+
+def create_star(db: Session, star: schemas.StarCreate) -> models.Star:
+    db_star = models.Star(name=star.name)
     db.add(db_star)
     db.commit()
     db.refresh(db_star)
     return db_star
 
 
-def create_director(db: Session, director: schemas.DirectorCreate):
-    db_director = models.Director(**director.dict())
+# --- Directors ---
+
+def get_director(db: Session, director_id: int) -> Optional[models.Director]:
+    return db.query(models.Director).filter(models.Director.id == director_id).first()
+
+
+def get_directors(db: Session, skip: int = 0, limit: int = 100) -> List[models.Director]:
+    return db.query(models.Director).offset(skip).limit(limit).all()
+
+
+def create_director(db: Session, director: schemas.DirectorCreate) -> models.Director:
+    db_director = models.Director(name=director.name)
     db.add(db_director)
     db.commit()
     db.refresh(db_director)
     return db_director
 
 
-def create_movie(db: Session, movie: schemas.MovieCreate):
+# --- Certifications ---
+
+def get_certification(db: Session, certification_id: int) -> Optional[models.Certification]:
+    return db.query(models.Certification).filter(models.Certification.id == certification_id).first()
+
+
+def get_certifications(db: Session, skip: int = 0, limit: int = 100) -> List[models.Certification]:
+    return db.query(models.Certification).offset(skip).limit(limit).all()
+
+
+def create_certification(db: Session, certification: schemas.CertificationCreate) -> models.Certification:
+    db_cert = models.Certification(name=certification.name)
+    db.add(db_cert)
+    db.commit()
+    db.refresh(db_cert)
+    return db_cert
+
+
+# --- Movies ---
+
+def get_movie(db: Session, movie_id: int) -> Optional[models.Movie]:
+    return db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+
+
+def get_movie_by_uuid(db: Session, movie_uuid: str) -> Optional[models.Movie]:
+    return db.query(models.Movie).filter(models.Movie.uuid == movie_uuid).first()
+
+
+def get_movies(db: Session, skip: int = 0, limit: int = 100) -> List[models.Movie]:
+    return db.query(models.Movie).offset(skip).limit(limit).all()
+
+
+def create_movie(db: Session, movie: schemas.MovieCreate) -> models.Movie:
     db_movie = models.Movie(
         name=movie.name,
         year=movie.year,
@@ -47,10 +103,13 @@ def create_movie(db: Session, movie: schemas.MovieCreate):
         price=movie.price,
         certification_id=movie.certification_id,
     )
-
-    db_movie.genres = db.query(models.Genre).filter(models.Genre.id.in_(movie.genre_ids)).all()
-    db_movie.stars = db.query(models.Star).filter(models.Star.id.in_(movie.star_ids)).all()
-    db_movie.directors = db.query(models.Director).filter(models.Director.id.in_(movie.director_ids)).all()
+    # Add relations
+    if movie.genre_ids:
+        db_movie.genres = db.query(models.Genre).filter(models.Genre.id.in_(movie.genre_ids)).all()
+    if movie.star_ids:
+        db_movie.stars = db.query(models.Star).filter(models.Star.id.in_(movie.star_ids)).all()
+    if movie.director_ids:
+        db_movie.directors = db.query(models.Director).filter(models.Director.id.in_(movie.director_ids)).all()
 
     db.add(db_movie)
     db.commit()
@@ -58,37 +117,24 @@ def create_movie(db: Session, movie: schemas.MovieCreate):
     return db_movie
 
 
-def get_movie(db: Session, movie_id: int):
-    return db.query(models.Movie).filter(models.Movie.id == movie_id).first()
-
-
-def get_movies(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Movie).offset(skip).limit(limit).all()
-
-
-def update_movie(db: Session, movie_id: int, movie_data: schemas.MovieUpdate):
-    movie = get_movie(db, movie_id)
-    if not movie:
-        return None
-
-    for field, value in movie_data.dict().items():
-        if field.endswith('_ids'):
-            continue
-        setattr(movie, field, value)
-
-    movie.genres = db.query(models.Genre).filter(models.Genre.id.in_(movie_data.genre_ids)).all()
-    movie.stars = db.query(models.Star).filter(models.Star.id.in_(movie_data.star_ids)).all()
-    movie.directors = db.query(models.Director).filter(models.Director.id.in_(movie_data.director_ids)).all()
-
+def update_movie(db: Session, movie: models.Movie, movie_update: schemas.MovieUpdate) -> models.Movie:
+    for field, value in movie_update.dict(exclude_unset=True).items():
+        if field in {"genre_ids", "star_ids", "director_ids"}:
+            if value is None:
+                continue
+            if field == "genre_ids":
+                movie.genres = db.query(models.Genre).filter(models.Genre.id.in_(value)).all()
+            elif field == "star_ids":
+                movie.stars = db.query(models.Star).filter(models.Star.id.in_(value)).all()
+            elif field == "director_ids":
+                movie.directors = db.query(models.Director).filter(models.Director.id.in_(value)).all()
+        else:
+            setattr(movie, field, value)
     db.commit()
     db.refresh(movie)
     return movie
 
 
-def delete_movie(db: Session, movie_id: int):
-    movie = get_movie(db, movie_id)
-    if movie:
-        db.delete(movie)
-        db.commit()
-        return True
-    return False
+def delete_movie(db: Session, movie: models.Movie) -> None:
+    db.delete(movie)
+    db.commit()
