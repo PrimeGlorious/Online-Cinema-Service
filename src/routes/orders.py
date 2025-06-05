10b7e7ceabc0a623365e03ac36e38df75618ac24
Andrii <yurkivandriy02@gmail.com>
2025-06-05 16:48:37 +0300

    Initial implementation of order creation endpoint

---
+++ src/routes/orders.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+
+from sqlalchemy import select
+
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from database import get_db
+from database.models.orders import OrderItem, OrderModel, OrderStatusEnum
+from notifications import EmailSenderInterface
+from schemas.orders import OrderCreateSchema, OrderReadSchema
+from services.orders import get_user_movies_by_order_status
+
+router = APIRouter()
+
+
+@router.post(
+    "/orders/",
+    response_model=OrderReadSchema,
+    summary="Order Creation",
+    description="Create a new order.",
+    status_code=status.HTTP_201_CREATED
+)
+async def register_user(
+        data: OrderCreateSchema,
+        db: AsyncSession = Depends(get_db),
+        current_user: UserModel = Depends(get_current_user)
+) -> OrderReadSchema:
+    cart_stmt = await db.execute(
+        select(CartItemModel.movie_id)
+        .join(CartModel)
+        .where(CartModel.user_id == current_user.id)
+    )
+    cart_movie_ids = cart_stmt.scalars().all()
+
+    if not cart_movie_ids:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Your shopping cart is empty."
+        )
+
+    bought_movie_ids = await get_user_movies_by_order_status(db=db, user_id=current_user.id, status=OrderStatusEnum.PAID)
+
+    for movie_id in data.movie_ids:
+        if movie_id not in cart_movie_ids:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Movie ID {movie_id} is not in your cart."
+            )
+
+    movie_ids_to_order = [movie_id for movie_id in data.movie_ids if movie_id not in bought_movie_ids]
+
+    if not movie_ids_to_order:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"All selected movies were already purchased."
+        )
+
+    pending_movie_ids = await get_user_movies_by_order_status(db=db, user_id=current_user.id, status=OrderStatusEnum.PENDING)
+
+    s_movie_ids_to_order, s_pending_movie_ids = set(movie_ids_to_order), set(pending_movie_ids)
+    conflict_ids = s_movie_ids_to_order.intersection(s_pending_movie_ids)
+
+    if conflict_ids:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Some movies already have pending orders: {list(conflict_ids)}"
+        )
+
+    stmt_price = await db.execute(select(MovieModel.price).where(MovieModel.id.in_(movie_ids_to_order)))
+    movie_price_map = {movie_id: price for movie_id, price in stmt_price.all()}
+    total_amount = sum(movie_price_map.values())
+
+    order = OrderModel(
+        user_id=current_user.id,
+        status=OrderStatusEnum.PENDING,
+        created_at=datetime.utcnow(),
+        total_amount=total_amount
+    )
+    db.add(order)
+    await db.flush()
+
+    order.items = [
+        OrderItem(
+            order_id=order.id,
+            movie_id=movie_id,
+            price_at_order=movie_price_map[movie_id]
+        )
+        for movie_id in movie_ids_to_order
+    ]
+    await db.commit()
+    await db.refresh(order)
+
+    return OrderReadSchema.from_orm(order)
+
