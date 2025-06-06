53a454a35b676f64dffd2ec39fd0b5b151f8eaa9
PrimeGlorious <TheComboPunk@gmail.com>
2025-06-07 01:18:36 +0300

    add handling for RateLimitExceeded exception

--- src/main.py
+++ src/main.py
@@ -16,6 +16,12 @@ app = FastAPI(
 
 limiter = Limiter(key_func=get_remote_address)
 
+
+@app.exception_handler(RateLimitExceeded)
+async def ratelimit_error(request: Request, exc: RateLimitExceeded):
+    return HTTPException(status_code=429, detail="Too many requests, please try again later.")
+
+
 api_version_prefix = "/api/v1"
 
 app.include_router(movies_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
