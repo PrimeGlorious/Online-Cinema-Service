4c732cff8b55e1d1930b1ab2bca7f6c854a5165a
AndyStar <andy_star@ukr.net>
2025-06-07 11:11:46 +0300

    Carts router added

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from custom.logger import setup_logger
 from routes.movies import router as movies_router
+from routes.carts import router as carts_router
 
 
 setup_logger()
@@ -14,3 +15,4 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(movies_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+app.include_router(carts_router, prefix=f"{api_version_prefix}/cart", tags=["theater"])
