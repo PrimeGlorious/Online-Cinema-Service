633915e9d43d95433373de4e1ad0460064fea164
Dmytro Stehantsev <stegan296@gmail.com>
2025-06-09 20:05:28 +0300

    fix EmailSender

--- src/tasks/emails.py
+++ src/tasks/emails.py
@@ -5,25 +5,12 @@ import asyncio
 
 from database import ActivationTokenModel
 from database.session_postgresql import SessionLocal
-from notifications.emails import EmailSender
 from config.dependencies.core import get_settings
 from tasks.celery import broker_url
+from config.dependencies.core import get_accounts_email_notificator
 
 print("BROKER_URL (emails.py):", broker_url)
 
-def get_sender(settings):
-    return EmailSender(
-        hostname=settings.EMAIL_HOST,
-        port=settings.EMAIL_PORT,
-        email=settings.EMAIL_HOST_USER,
-        password=settings.EMAIL_HOST_PASSWORD,
-        use_tls=settings.EMAIL_USE_TLS,
-        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
-        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
-        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
-        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
-        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,
-    )
 
 @shared_task
 def send_activation_email_task(email: str, activation_link: str):
@@ -31,7 +18,7 @@ def send_activation_email_task(email: str, activation_link: str):
     Celery task to send account activation email using EmailSender.
     """
     settings = get_settings()
-    sender = get_sender(settings)
+    sender = get_accounts_email_notificator(settings)
     # Run async send_activation_email in sync context
     asyncio.run(sender.send_activation_email(email, activation_link))
 
@@ -39,7 +26,7 @@ def send_activation_email_task(email: str, activation_link: str):
 @shared_task
 def send_activation_complete_email_task(email: str, login_link: str):
     settings = get_settings()
-    sender = get_sender(settings)
+    sender = get_accounts_email_notificator(settings)
     asyncio.run(sender.send_activation_complete_email(email, login_link))
 
 
